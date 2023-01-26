2c0d4deb29bfce53c4145a21c4744e0e56e8c5c1
Emerald <emerald_actual@proton.me>
2023-01-25 21:23:43 -0500

    switch to libloading from winapi

--- src/launcher.rs
+++ src/launcher.rs
@@ -1,5 +1,24 @@
-use winapi::um::LoadLibraryExW;
+use libloading::{Symbol, Library};
+use anyhow::Result;
+use lazy_static::lazy_static;
+use std::path::PathBuf;
 
-pub fn load_northstar() -> Result<(), String> {
+lazy_static!{
+    static ref EXE_DIR: PathBuf = std::env::current_exe().expect("Unable to get the path to the executable").parent().expect("Executable path has no parent dir").to_path_buf(); 
+    // needs to be static so that it doesn't get dropped until the end of the program
+    static ref TIER0: Result<Library, libloading::Error> = unsafe { Library::new(EXE_DIR.join("bin").join("x64_retail").join("tier0.dll")) };
+}
+
+pub fn init() -> Result<()> {
+    load_tier0()?;
+
     Ok(())
+}
+
+fn load_tier0() -> Result<()> {
+    if let Err(e) = &*TIER0 {
+        Err(e.into())
+    } else {
+        Ok(())
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,14 @@
+// this is here because I don't want it in the dynamic lib and there isn't currently
+// support for coniditional compilation on the crate-type
 mod launcher;
 
+#[cfg(debug_assertions)]
 fn main() {
-    launcher::load_northstar().expect("Load failed");
+    let res = launcher::init();
+    println!("{:?}", res);
+}
+
+#[cfg(not(debug_assertions))]
+fn main() {
+    todo!();
 }
